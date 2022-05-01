ff2fc2947fba5f69347e1383b15f4d4d103a2831
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2022-05-01 12:05:08 +0200

    Correction bug database + adding documentation

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,20 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+
+/* /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+I did not use the "Route::view" feature because I prefer to have a global overview of all my routes, and middleware,
+here. So I voluntarily implemented all the routes here
+/* /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/*/
 
 Route::get('/',  [VisitorController::class, 'view'])->name('main-home');
 
 Auth::routes();
 
-
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/profil', [ProfilController::class, 'index'])->name('profil');
-
-    // ->middleware('isLibrarian')
 
 
     Route::put('/borrow/edit/{id}', [BorrowController::class, 'edit'])->name('borrow-edit')->middleware('isLibrarian');
@@ -40,14 +36,11 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/rentals/manage-all-rentals',  [BorrowController::class, 'librarianManageAllRentals'])->name('manage-all-rentals')->middleware('isLibrarian');
     Route::get('/rentals/{id}', [BorrowController::class, 'select'])->name('rental');
 
-
     Route::get('/book/create', [BookController::class, 'create'])->name('create-book')->middleware('isLibrarian');
     Route::get('/book/edit/{id}', [BookController::class, 'editPage'])->name('edit-book')->middleware('isLibrarian');
     Route::put('/book/update/{id}', [BookController::class, 'update'])->name('update-book')->middleware('isLibrarian');
     Route::post('/book/store', [BookController::class, 'store'])->name('store-book')->middleware('isLibrarian');
     Route::delete('/book/{id}', [BookController::class, 'destroy'])->name('delete-book')->middleware('isLibrarian');
-
-
 
     Route::get('/genres', [GenreController::class, 'index'])->name('genres')->middleware('isLibrarian');
     Route::delete('/genres/destroy/{id}', [GenreController::class, 'destroy'])->name('delete-genre')->middleware('isLibrarian');
@@ -58,8 +51,6 @@ Route::middleware(['auth'])->group(function () {
 
 });
 
-
-// Route::get('/books-genre/{id}', [BookController::class, 'byGenre'])->name('books-by-genre');
 Route::get('/books-genre/{id}', [BookController::class, 'byGenre'])->name('books-by-genre');
 Route::get('/books', [BookController::class, 'all'])->name('books');
 Route::get('/book-search/', [BookController::class, 'search'])->name('book-search');
